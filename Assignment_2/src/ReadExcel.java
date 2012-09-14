9895f7e8ffa041d3d623d5dd2a913be55aa34acf
Ankit <ankit@ubuntu.ubuntu-domain>
2012-09-14 20:10:17 +0530

    Excel Reader working

--- Assignment_2/src/ReadExcel.java
+++ Assignment_2/src/ReadExcel.java
@@ -5,35 +5,28 @@ import jxl.CellType;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
+
 public class ReadExcel {
 	private String DATA_FILE;
 	public void setInputFile(String file_name) {
 	    this.DATA_FILE = file_name;	    
 	}
-	 public void read() throws IOException  {
+	public void read() throws IOException  {
 		    File inputWorkbook = new File(DATA_FILE);
 		    Workbook w;
 		    try {
 		      w = Workbook.getWorkbook(inputWorkbook);
 		      // Get the first sheet
-		      Sheet sheet = w.getSheet(0);
-		      // Loop over first 10 column and lines
-		      for (int j = 0; j < sheet.getColumns(); j++) {
-		        for (int i = 0; i < sheet.getRows(); i++) {
-		          Cell cell = sheet.getCell(j, i);
+		      Sheet sheet = w.getSheet(0);      
+		      for (int j = 0; j < sheet.getRows(); j++) {
+		        for (int i = 0; i < sheet.getColumns(); i++) {
+		          Cell cell = sheet.getCell(i,j);
 		          CellType type = cell.getType();
-		          if (cell.getType() == CellType.LABEL) {
-		            System.out.println("I got a label "
-		                + cell.getContents());
-		          }
-		          if (cell.getType() == CellType.NUMBER) {
-		            System.out.println("I got a number "
-		                + cell.getContents());
-		          }
+		          System.out.println(cell.getContents());
 		        }
 		      }
 		    } catch (BiffException e) {
 		      e.printStackTrace();
 		    }
-		  }
+	 }
 }

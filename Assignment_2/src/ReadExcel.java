a7f327579d7fcbff0a893acf6fa75e51d0fb1bdb
Ankit <ankit@ubuntu.ubuntu-domain>
2012-09-15 18:14:10 +0530

    Added nodes.Parties not included

--- Assignment_2/src/ReadExcel.java
+++ Assignment_2/src/ReadExcel.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.*;
 import java.io.IOException;
 
 import prefuse.data.Graph;
@@ -32,14 +33,41 @@ public class ReadExcel {
 		      // Get the first sheet
 		      Sheet sheet = w.getSheet(0);  
 		  	  node = tree.addNode();
-		  	  node.set("label","India");		  
+		  	  node.set("label","India");	
+	    	  HashMap<String,Integer> hm = new HashMap<String,Integer>();		  	  
 		      for (int j = 1; j < sheet.getRows(); j++) {
-		      //  for (int i = 0; i < sheet.getColumns(); i++) {
-		          Cell cell = sheet.getCell(4,j);
-		          CellType type = cell.getType();
-		          System.out.println(cell.getContents());
+		   	  //  for (int i = 0; i < sheet.getColumns(); i++) {
+		          Cell cell = sheet.getCell(4,j);		          
+		          hm.put(cell.getContents(),-1);
 		      //  }
 		      }
+		       Set set = hm.entrySet();
+			   // Get an iterator
+			   Iterator i = set.iterator();
+			   int index=0;
+			   // Display elements
+			   while(i.hasNext()) {
+				   Map.Entry me = (Map.Entry)i.next();
+				   node = tree.addNode();
+				   Node parent = tree.getNode(0);
+				   Edge e = tree.addEdge(parent,node);
+				   node.set("label", me.getKey());
+				   hm.put(me.getKey().toString(),++index);
+			   } 
+			   for(int j = 1;j <sheet.getRows(); j++)
+			   {
+				   Cell state = sheet.getCell(4,j);
+				   Cell district = sheet.getCell(5,j);
+				   Cell name = sheet.getCell(0,j);
+				   int state_index = hm.get(state.getContents());
+				   Node district_node = tree.addNode();
+				   Node state_node = tree.getNode(state_index);
+				   Node politician = tree.addNode();
+				   tree.addEdge(state_node, district_node);
+				   tree.addEdge(district_node, politician);
+				   district_node.set("label",district.getContents());
+				   politician.set("label",name.getContents());				   
+			   }
 		    } catch (BiffException e) {
 		      e.printStackTrace();
 		    }

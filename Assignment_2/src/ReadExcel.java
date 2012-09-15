48f33e9564147253308dc58d8813085029d143bf
Ankit <ankit@ubuntu.ubuntu-domain>
2012-09-15 16:40:38 +0530

    read() now returns a tree

--- Assignment_2/src/ReadExcel.java
+++ Assignment_2/src/ReadExcel.java
@@ -1,5 +1,11 @@
 import java.io.File;
 import java.io.IOException;
+
+import prefuse.data.Graph;
+import prefuse.data.Tree;
+import prefuse.data.Edge;
+import prefuse.data.Node;
+import prefuse.data.Table;
 import jxl.Cell;
 import jxl.CellType;
 import jxl.Sheet;
@@ -11,22 +17,32 @@ public class ReadExcel {
 	public void setInputFile(String file_name) {
 	    this.DATA_FILE = file_name;	    
 	}
-	public void read() throws IOException  {
+	public Tree read() throws IOException  {
+	        Table nodeData = new Table();
+	        Table edgeData = new Table();
+	        nodeData.addColumn("label", String.class);
+	        edgeData.addColumn(Tree.DEFAULT_SOURCE_KEY, int.class);
+	        edgeData.addColumn(Tree.DEFAULT_TARGET_KEY, int.class);
+	  	    Tree tree = new Tree(nodeData,edgeData);			
 		    File inputWorkbook = new File(DATA_FILE);
 		    Workbook w;
 		    try {
+		      Node node = null;
 		      w = Workbook.getWorkbook(inputWorkbook);
 		      // Get the first sheet
-		      Sheet sheet = w.getSheet(0);      
-		      for (int j = 0; j < sheet.getRows(); j++) {
-		        for (int i = 0; i < sheet.getColumns(); i++) {
-		          Cell cell = sheet.getCell(i,j);
+		      Sheet sheet = w.getSheet(0);  
+		  	  node = tree.addNode();
+		  	  node.set("label","India");		  
+		      for (int j = 1; j < sheet.getRows(); j++) {
+		      //  for (int i = 0; i < sheet.getColumns(); i++) {
+		          Cell cell = sheet.getCell(4,j);
 		          CellType type = cell.getType();
 		          System.out.println(cell.getContents());
-		        }
+		      //  }
 		      }
 		    } catch (BiffException e) {
 		      e.printStackTrace();
 		    }
+		    return tree;
 	 }
 }

f7978fbaa6a1879d3f0c267c9337771daa426675
Ankit <ankit@ubuntu.ubuntu-domain>
2012-10-01 09:56:49 +0530

    Adding struct class

---
+++ Assignment_2/src/Assignment2/visualizations/CityStruct.java
@@ -0,0 +1,11 @@
+package Assignment2.visualizations;
+
+public class CityStruct {
+	public String state;
+	public String party;
+	public String candidate;
+	public String gender;
+	public String age;
+	public String education;
+	public String attendance;
+}
